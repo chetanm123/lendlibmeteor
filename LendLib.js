4bc4208d699434784f546a73ecce3cb6d3f4aa37
Chetan <chetan@vouchercodesuae.com>
2014-09-12 20:11:27 +0530

    View Model

--- LendLib.js
+++ LendLib.js
@@ -1,6 +1,29 @@
 lists = new Meteor.Collection("Lists");
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
+	
+	Template.categories.lists=function(){
+	  return lists.find({},{sort:{Category:1}});
+	};
+
+	//We are declaring the adding_category flag
+	Session.set('adding_category',false);
+
+	//this returns true if adding_category has been assigned a value true
+
+	Template.categories.new_cat=function(){
+		console.log("then here");
+		return Session.equals('adding_category',true);
+	};
+
+	Template.categories.events({
+		'click #btnNewCat':function(e,t){
+			
+			Session.set('adding_category',true);
+			Meteor.flush();
+			focusText(t.find("#add-category"));
+		}
+	});
+ /* Template.hello.greeting = function () {
     return "My list.";
   };
 
@@ -10,15 +33,18 @@ if (Meteor.isClient) {
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
     }
-  });
+  });*/
 
-  Template.categories.lists=function(){
-	  return lists.find({},{sort:{Category:1}});
-  };
+	function focusText(i){
+		i.focus();
+		i.select();
+	}
 }
 
 if (Meteor.isServer) {
+	
   Meteor.startup(function () {
+	
     // code to run on server at startup
   });
 }
